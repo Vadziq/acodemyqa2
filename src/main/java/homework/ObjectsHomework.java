d0c72bbbfe2663a7afec7706d87f7acba5114031
vadziq <vadziq@gmail.com>
2022-01-26 18:57:23 +0200

    [feature/java4]: Delete unused code parts

--- src/main/java/homework/ObjectsHomework.java
+++ src/main/java/homework/ObjectsHomework.java
@@ -1,16 +1,15 @@
 package homework;
-import java.util.Scanner;
+
 
 public class ObjectsHomework {
     public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
+
         Animal dog = new Animal("Dog",false, 4, 4);
         Animal cat = new Animal("Cat",false, 4, 4);
         Animal cow = new Animal("Cow",false, 4, 4);
         Animal chicken = new Animal("Chicken",false, 4, 2);
         Animal sheep = new Animal("Sheep",false, 4, 4);
         Animal pig = new Animal("Pig",false, 4, 4);
-        Animal df = new Animal("fg");
 
         dog.status();
         dog.goSleep();
