d569c51cdad1f3ae41e396446203bf4171e755cc
vadziq <vadziq@gmail.com>
2022-02-01 02:31:07 +0200

    [feature/javaTest]: Add RecursionBase.java and RecursionTest.java

--- src/main/java/homework/RecursionBase.java
+++ src/main/java/homework/RecursionBase.java
@@ -4,6 +4,7 @@ public class RecursionBase {
     public static void main(String[] args) {
         System.out.println(factorialum(5));
     }
+
     private static int factorialum(int x) {
         if (x == 1) {
             return 1;
--- src/main/java/homework/RecursionTest.java
+++ src/main/java/homework/RecursionTest.java
@@ -4,6 +4,7 @@ public class RecursionTest {
     public static void main(String[] args) {
         prmt(0);
     }
+
     private static void prmt(int i) {
         for (int j = i; j <= 2; j++) {
             System.out.println("i " + i + " j " + j);
