b8018ff34b1a67db64c1a6a3b1bb4c932a13e65c
vadziq <vadziq@gmail.com>
2022-02-24 22:42:41 +0200

    [feature/SeleniumTest2]: Add webdrivermanager

--- src/main/java/utils/LocalDriverManager.java
+++ src/main/java/utils/LocalDriverManager.java
@@ -1,7 +1,10 @@
 package utils;
 
+import exceptions.UnsupportedDriverException;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LocalDriverManager {
 
@@ -9,9 +12,23 @@ public class LocalDriverManager {
 
     public static WebDriver getInstance(){
         if(driver == null){
-            driver = new ChromeDriver();
+            driver = localDriver();
         }
         return driver;
+    }
+
+    public static WebDriver localDriver(){
+        String browser = PropertiesReader.getProperties().getProperty("browser").toLowerCase();
+        switch (browser){
+            case"chrome":
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case"firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+
+            default: throw new UnsupportedDriverException("Following browser is not supported");
+        }
     }
 
     public static void closeDriver(){
